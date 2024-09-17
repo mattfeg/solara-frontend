5ae469e497af28dc3db1d46a363609dc474260e7
Matheus Ferreira <ferreira@MacBook-Air-de-Matheus.local>
2024-09-17 18:50:09 -0300

    Update Homepage layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import '@/app/styles/globals.css';
-import { ReactNode } from 'react';
+import "@/app/styles/globals.css";
+import { ReactNode } from "react";
 
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const RootLayout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-        <html lang="pt-BR">
-        <head>
-            <link rel="icon" type="image/png" href="/assets/solara.png"/>
-            <title>Solara</title>
-        </head>
-        <body className="bg-sky-600">
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" type="image/png" href="/assets/solara.png" />
+        <title>Solara</title>
+      </head>
+      <body className="bg-sky-600 min-h-screen overflow-hidden">
         <main className="p-10">{children}</main>
         <footer className="text-white text-center py-4 mt-8 pt-20">
-            <p>© 2024 Solara. All rights reserved.</p>
+          <p>© 2024 Solara. All rights reserved.</p>
         </footer>
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
